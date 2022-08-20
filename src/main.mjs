b342bb7fd07c9d9d7dcd75351121a669f497d34c
Ospinajuanp <juanpo996@gmail.com>
2022-08-20 14:17:01 -0500

    Correcion index.html

--- src/main.mjs
+++
@@ -1,3 +0,0 @@
-import { API_URL, API_KEY} from './secrets.mjs';
-
-console.log(API_KEY)
--- src/secrets.mjs
+++
@@ -1,2 +0,0 @@
-export const API_KEY = 'bce73a49412529da65e61a49a9407e8e';
-export const API_URL= `https://api.themoviedb.org/3/`;
